6995748749a5ea52b2b1b2259168e007ef85ff26
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-01-10 16:29:10 +0530

    Create evena.c

---
+++ evena.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,f,l;
+printf("enter the no.1st interval");
+scanf("%d",&f);
+printf("enter the no. 2nd interval");
+scanf("%d",&l);
+for(i=f;i<=l;i++)
+{
+if(i%2==1)
+{
+printf("%d is an addno.",i);
+}
+}
+return o;
+}

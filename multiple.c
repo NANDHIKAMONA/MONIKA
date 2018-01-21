72020a9b277bcbcfb817bdc9b162b7bea4b0f56c
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-01-21 10:28:20 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+logint sum=0,i,n;
+clrscr;
+print("|n give the value of n:");
+scanf("%1d",@n);
+for(i=1;i<=n;i++)
+{
+printf("\n 5* %1d = %1d",i,5*i);
+}
+getch();
+}

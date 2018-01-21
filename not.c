240317b94dc081465b515ba5b5a1e2e68119041a
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-01-21 09:53:19 +0530

    Create not.c

---
+++ not.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+printf("enter 1st integer number");
+scanf("%d",&n);
+printf("enter 2nd integer number");
+scanf("%d",&n);
+{
+if(n%i==0)
+{
+break
+}
+}
+if(i>n\2)
+printf("%d yes if it  is prime",n);
+else
+printf("% no if it is not a prime",n);

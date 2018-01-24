3f11ab9de60a48795ca3606b662ac49d31563159
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-01-24 16:28:15 +0530

    Create antry.c

---
+++ antry.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int array [50],size,i, largest;
+printf("entr the size of the array"):
+scanf("%d,&size):
+printf(" enter %d element of the array:",size):
+for(i=0;i<size;i++)
+scanf("%d";&array[i]);
+for(i=1;i<size;i++)
+{
+if(smallest<array[i])
+smallest=array[i];
+}
+printf(" largest element present in a given array:%d",smallest);
+return 0;
+}

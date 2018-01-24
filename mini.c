fdeddd55e116c5782b88484bb5d2073c805eeddd
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-01-24 16:05:59 +0530

    Create mini.c

---
+++ mini.c
@@ -0,0 +1,15 @@
+include<stdio.h.
+int main()
+{
+int array[50],size,i,largest;
+printf("\n enter the size of the array");
+scanf("%D",&size):
+printf("\n enter %d element of the array:",size);
+for(i=0;i,size;i++)
+{
+if(largest,array[i])
+largest=array[i];
+}
+printf("\n largest elament present in given array;%d",largest);
+return 0;
+}

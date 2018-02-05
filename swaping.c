c75320bff8f98b2eba4849c202cf69a99548d433
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-02-05 10:46:53 +0000

    Create swaping.c

---
+++ swaping.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=5,b=8,t;
+    printf("value of 'a' and 'b' before swaping%d%d",a,b);
+    t=a;
+    a=b;
+    b=t;
+    printf("value of 'a' and 'b' after swaping%d%d",a,b);
+    return 0;
+}
